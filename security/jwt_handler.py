f6ed89d386cbb55e55bd27a2a6c8e6077a601a9b
judahtc <jaycea247@gmail.com>
2024-11-18 20:38:42 +0200

    created the create access token function

---
+++ security/jwt_handler.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+from decouple import config
+import jwt
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt.exceptions import InvalidTokenError
+from passlib.context import CryptContext
+from pydantic import BaseModel
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, config(
+        "secret"), algorithm=config("algorithm"),)
+    return encoded_jwt
