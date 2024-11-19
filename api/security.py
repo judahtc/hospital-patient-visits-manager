f4d9b881279d4ccc9780af4953c944fb58e0c678
judahtc <jaycea247@gmail.com>
2024-11-19 08:22:21 +0200

    added password hashing and logging in with a hashed password

--- api/security.py
+++ api/security.py
@@ -7,19 +7,23 @@ from db.database import SessionLocal, engine, get_db
 from sqlalchemy.orm import Session
 from typing import List, Union
 import sqlalchemy
+from jwt.exceptions import InvalidTokenError
+from passlib.context import CryptContext
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 
 @router.post("/login")
 def login(user: str, login_data: schemas.LoginSchema, db: Session = Depends(get_db)):
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
     if user == "admin":
         admin = db.query(models.Admin).filter(
             models.Admin.email == login_data.email).first()
         if not admin:
             raise HTTPException(status_code=404, detail="User not found")
-        if admin.password != login_data.password:
-            raise HTTPException(status_code=401, detail="Incorrect password")
-        return create_access_token(login_data)
+        if pwd_context.verify(login_data.password, admin.password):
+            return create_access_token(login_data)
+        raise HTTPException(status_code=401, detail="Incorrect password")
+
     else:
         patient = db.query(models.Patient).filter(
             models.Patient.email == login_data.email).first()
