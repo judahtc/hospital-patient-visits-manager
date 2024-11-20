35bb5e8d23835afcc316399c97e05c51986d6db0
judahtc <jaycea247@gmail.com>
2024-11-20 14:36:40 +0200

    feat: added jwt token decoding function

--- security/jwt_handler.py
+++ security/jwt_handler.py
@@ -7,6 +7,7 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jwt.exceptions import InvalidTokenError
 from passlib.context import CryptContext
 from pydantic import BaseModel
+import time
 
 
 def create_access_token(data: dict):
@@ -19,3 +20,15 @@ def create_access_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, config(
         "secret"), algorithm=config("algorithm"))
     return {"access_token": encoded_jwt}
+
+
+def decodeJWT(token: str) -> dict:
+    JWT_ALGORITHM = config("algorithm")
+    JWT_SECRET = config("secret")
+
+    try:
+        decoded_token = jwt.decode(
+            token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        return decoded_token if decoded_token["expires"] >= time.time() else None
+    except:
+        return {"response": "token expired"}
